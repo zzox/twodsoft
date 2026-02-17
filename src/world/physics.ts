c79024d20b6af39fe77c9af89459607ac91be4f1
zzox <ty@zzox.net>
2026-02-16 22:27:32 -0600

    prevent seam clipping when not pressed

--- src/world/physics.ts
+++ src/world/physics.ts
@@ -9,7 +9,8 @@ export const updatePhysics = (actor:Actor) => {
 
 // Returns true if two physics bodies overlap.
 export const overlaps = (x1:number, y1:number, w1:number, h1:number, x2:number, y2:number, w2:number, h2:number):boolean =>
-  x1 + w1 >= x2 && x1 <= x2 + w2 && y1 + h1 >= y2 && y1 <= y2 + h2
+  x1 + w1 > x2 && x1 < x2 + w2 && y1 + h1 > y2 && y1 < y2 + h2
+  // x1 + w1 >= x2 && x1 <= x2 + w2 && y1 + h1 >= y2 && y1 <= y2 + h2 <- seam clips
 
 export const collideWall = (actor:Actor, wx:number, wy:number, ww:number, wh:number) => {
   if (overlaps(actor.pos.x, actor.pos.y, actor.size.x, actor.size.y, wx, wy, ww, wh)) {   
