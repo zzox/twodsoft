a8f6a46670bb96d6ba5dd0bb67a99b6e4361c013
Tyler <ty@zzox.net>
2026-02-23 14:40:24 -0600

    better anims for guy

--- src/data/anim-data.ts
+++ src/data/anim-data.ts
@@ -1,4 +1,4 @@
-import { Thing, ThingState, ThingType } from './actor-data'
+import { Actor, Thing, ThingState, ThingType } from './actor-data'
 
 type Anim = {
   repeats:boolean
@@ -13,6 +13,12 @@ ballAnims.set(ThingState.None, { repeats: false, frames: [0], speed: 1 })
 ballAnims.set(ThingState.Moving, { repeats: true, frames: [0, 1], speed: 5 })
 anims.set(ThingType.Rock, ballAnims)
 
+const guyAnims = new Map()
+guyAnims.set(ThingState.None, { repeats: false, frames: [0], speed: 1 })
+guyAnims.set(ThingState.Moving, { repeats: true, frames: [0, 1, 0, 2], speed: 10 })
+guyAnims.set(ThingState.PreThrow, { repeats: true, frames: [3, 4, 3, 5], speed: 12 })
+anims.set(ThingType.Guy, guyAnims)
+
 export const getAnim = (thing:Thing):number => {
   const anim = anims.get(thing.type)!.get(thing.state)!
   if (anim.repeats) {
@@ -22,4 +28,17 @@ export const getAnim = (thing:Thing):number => {
   }
 }
 
-export { anims }
+export const getActorAnim = (actor:Actor):number => {
+  let anim = anims.get(actor.type)!.get(ThingState.None)!
+  if (actor.state === ThingState.PreThrow) {
+    anim = anims.get(actor.type)!.get(ThingState.PreThrow)!
+  } else if (actor.vel > 0) {
+    anim = anims.get(actor.type)!.get(ThingState.Moving)!
+  }
+
+  if (anim.repeats) {
+    return actor.type + actor.facing * actor.animsLength + anim.frames[Math.floor(actor.stateTime / anim.speed) % anim.frames.length]
+  } else {
+    return actor.type + actor.facing * actor.animsLength + anim.frames[Math.min(Math.floor(actor.stateTime / anim.speed), anim.frames.length - 1)]
+  }
+}
